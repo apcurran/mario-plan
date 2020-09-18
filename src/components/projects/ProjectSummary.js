662333a08851194e2b44ae3909b14b31bcf842a8
Alex Curran <alexcurran.va@gmail.com>
2020-09-18 11:04:17 -0500

    Basic component setup

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ProjectSummary() {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by Alex Curran</p>
+                <p className="grey-text">September 3, 2 PM</p>
+            </div>
+        </div>
+    );
+}
