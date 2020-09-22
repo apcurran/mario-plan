e2261ed3127985f814e64a242da19394327a5cf2
Alex Curran <alexcurran.va@gmail.com>
2020-09-22 11:51:27 -0500

    Initial build subtree commit

---
+++ build/precache-manifest.2e70e4e04065b74a6db60cee04064cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e54fadf58f0e8193947345a7756660a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aeac6593f9b747324867",
+    "url": "./static/css/main.7efff914.chunk.css"
+  },
+  {
+    "revision": "4b5bd608781ffe210dfc",
+    "url": "./static/js/2.1c5ea13d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "./static/js/2.1c5ea13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeac6593f9b747324867",
+    "url": "./static/js/main.d105a6ea.chunk.js"
+  },
+  {
+    "revision": "57f569b0b201116bdaa1",
+    "url": "./static/js/runtime-main.a514e1e1.js"
+  }
+]);
